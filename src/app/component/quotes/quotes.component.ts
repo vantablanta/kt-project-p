c54b53f658677c5fe9e323137168ef806bbbbc8d
vanta blanta <vantablanta@gmail.com>
2022-04-09 13:45:06 +0300

    directive added

--- src/app/component/quotes/quotes.component.ts
+++ src/app/component/quotes/quotes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Quote} from 'src/app/quote'
+import { Quote } from 'src/app/quote'
 
 @Component({
   selector: 'app-quotes',
@@ -8,12 +8,17 @@ import {Quote} from 'src/app/quote'
 })
 export class QuotesComponent implements OnInit {
 
-
   quotes: Quote[] = [
-    new Quote(1, "First Quote", 0, 0),
-    new Quote(2, "Second Quote", 0, 0),
-    new Quote(3, "Third Quote", 0, 0)
+    new Quote(1, "First Quote", 5, 0),
+    new Quote(2, "Second Quote", 7, 0),
+    new Quote(3, "Third Quote", 1, 0)
   ]
+
+  arr: number[] = this.quotes.map(function (quote) {
+    return quote.upvotes
+  })
+  highest = Math.max(...this.arr)
+
   constructor() { }
 
   ngOnInit(): void {
